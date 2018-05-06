7845cf5986bc231529f887343e250cec6af103a6
PtrWlp <peter.welp@klm.com>
2018-05-06 13:28:01 +0200

    add header, wind and split columns in csv

--- metraocrawler.js
+++ metraocrawler.js
@@ -3,35 +3,59 @@
 var requestP = require('request-promise');
 var cheerio = require('cheerio');
 var fs = require('fs-extra');
+var requestSync = require('sync-request');
+var get = require('lodash.get');
 
 var uri = 'http://data3.eventacoustics.nl/elka31/n1kavel/live_per_station_graphdraw_public_enm.inc.php';
-var outfile = `output/metraocrawler.csv`;
-var interval = 3; // minutes 
 
-fs.outputFile(outfile, 'header;column', function (err) {
-    if (err) throw err;
-});
+var d = new Date().toISOString().slice(0,17).replace(/-/g,"").replace(/:/g,"").replace("T","-");
+var outfile = `output/metraocrawler-${d}.csv`;
+var interval = 1; // minutes 
+var headerHelper = true;
 
-function getMeasure() {
-    
+var knmiApiKey = '6772068fdf';
+var knmiLocation = 'Geuzenveld';
+var knmiUrl = `http://weerlive.nl/api/json-data-10min.php?key=${knmiApiKey}&locatie=${knmiLocation}`;
+
+function getMeasure(onetwothree) {
+
+    if (headerHelper) {
+        headerHelper = false;
+        // write header once
+        fs.outputFile(outfile, 'measurepoint,timestamp,dBA,dBC,norm,winddir,windspeed,temp,n\n', function (err) {
+            if (err) throw err;
+        });
+    }
+
     requestP(uriPlusOption(uri))
     .then(function ($) {
+        var res = requestSync('GET', knmiUrl);
+        var weather = JSON.parse(res.getBody('utf8'));
+        var temperature = get(weather, 'liveweer[0].temp', '?');
+        var winddir = get(weather, 'liveweer[0].windr', '?');
+        var windspeed = get(weather, 'liveweer[0].windms', '?');
+        var closingcolumns = `${winddir},${windspeed},${temperature},${onetwothree}`;
 
         var measures = $('.col_3.center > fieldset');
         measures.map(function() {
             var measure = $(this).html();
-            // console.log($(this).html());
-            // TODO parse into csv
-            fs.appendFile(outfile, measure, function (err) {
+            var measurepoint = $(this).find('fieldset h4').text();
+            var timestamp = $(this).find('fieldset h6').text().substr(11,5);
+            var decibels = $(this).find('table tr td h10').toArray();
+            var dBA = $(decibels[0]).text();
+            var dBC = $(decibels[1]).text();
+            var norm = $(decibels[2]).text();
+            
+            fs.appendFile(outfile, `${measurepoint},${timestamp},${dBA},${dBC},${norm},${closingcolumns}\n`, function (err) {
             });
             return $(this).text();
-        }).toArray();
+        });
 
               
     })
     .catch(function (err) {
         // Crawling failed or Cheerio choked... 
-        console.log('error index: ', err);
+        console.log(err);
         // or end of list
     });
 
@@ -47,9 +71,14 @@ function uriPlusOption(uri) {
     };
 }
 
+var onetwothree = 1; // deduplicate if we only get 1 measure every 3 minutes.
 function trigger() {
-    getMeasure();
+    onetwothree += 1;
+    if (onetwothree === 4) {
+        onetwothree = 1;
+    }
+    getMeasure(onetwothree);
 }
 // initial and every n minutes ( depends on interval, usually 5 or 3 )
-getMeasure();
+getMeasure(onetwothree);
 setInterval(trigger, interval*60000);
